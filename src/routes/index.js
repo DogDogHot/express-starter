fcab8107f060716514d5fa53e69be60ecab3fb00
DogDogHot <k6014014@gmail.com>
2022-04-08 22:40:49 +0900

    [hotfix] auth 라우터 미적용 버그 수정

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import userRoutes from "./user.route";
+import authRoutes from "./auth.route";
 
 const router = express.Router();
 
@@ -7,6 +8,10 @@ const defaultRoutes = [
   {
     path: "/users",
     route: userRoutes,
+  },
+  {
+    path: "/auth",
+    route: authRoutes,
   },
 ];
 
